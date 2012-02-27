92467228e83c6fb581aa910215df7deefb2a85bd
Jonathan Bourdett <jab2279@columbia.edu>
2012-02-26 22:31:51 -0500

    added matrix multiplication 2x2 times 2x2 and 2x2 times 2x1

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -33,7 +33,69 @@ public class Exercise3 {
 		System.out.println();
 	}
 	
-	public void run(){
+	/**
+	 * Calculate equivalence of C1(U) and C2(U^3)
+	 */
+	public boolean run(){
+		boolean equivalent = false;
 		
+		//TODO actual calculation
+//		Complex[] e = new Complex();
+//		
+//		double norm = norm(e);
+		
+		return equivalent;
+	}
+	
+	private Complex[][] calculateU3(){
+		Complex[][] u3 = new Complex[2][2];
+		
+		Complex[][] u2 = matrixProduct2by2(u, u);
+		u3 = matrixProduct2by2(u2, u);
+		
+		return u3;
+	}
+	
+	private Complex[][] matrixProduct2by2(Complex[][] m1, Complex[][] m2){
+		if (m1.length!=2 && m2.length!=2)
+			return null;
+		
+		Complex[][] product = new Complex[2][2];
+		for (int rowIndex=0; rowIndex<2; rowIndex++){
+			for (int columnIndex=0; columnIndex<2; columnIndex++){
+				for (int a=0; a<2; a++){
+					product[rowIndex][columnIndex] = m1[rowIndex][a].mul(m2[a][columnIndex]);
+				}
+			}
+		}
+		return product;
+	}
+	
+	private Complex[] matrixProduct2by1(Complex[][] m1, Complex[] m2){
+		if (m1.length!=2 && m2.length!=1)
+			return null;
+		
+		Complex[] product = new Complex[2];
+		for (int rowIndex=0; rowIndex<2; rowIndex++){
+			for (int a=0; a<2; a++){
+				product[rowIndex] = m1[rowIndex][a].mul(m2[a]);
+			}
+		}
+		
+		return product;
+	}
+	
+	private double norm(Complex[] vector){
+		double ret = 0;
+//		Complex complexRet = new Complex();
+		
+		for (int i=0; i<vector.length; i++){
+//			complexRet = complexRet.add((vector[i].getConjugate()).mul(vector[i]));
+			ret += (vector[i].getConjugate()).mul(vector[i]).getRe();
+		}
+		
+		ret = Math.sqrt(ret);
+		
+		return ret;
 	}
 }
