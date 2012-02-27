3449eb2d9aab740ec8e261493b88c971eed100f9
Jonathan Bourdett <jab2279@columbia.edu>
2012-02-26 19:13:52 -0500

    currently takes in input as integers correctly. but need to change to doubles

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,10 +1,31 @@
 
 public class Exercise3 {
 
-	int[][] u;
+	private double[][] u;
+	private boolean DEBUG = true;
 	
-	public Exercise3(int[][] u){
-		this.u = u;
+//	public Exercise3(double[][] u){
+//		this.u = u;
+//	}
+	
+	public Exercise3(double[] input){
+		u = new double[2][2];
+		
+		int a = 0;
+		int b = 0;
+		for (int i=0; i<input.length; i++){
+			if (b>=2){
+				a++;
+				b=0;
+			}
+			u[a][b++] = input[i];
+			if (DEBUG == true){
+				//print out 
+				System.out.println("u_" + a + (b-1) + " = " + u[a][b-1]);
+			}
+		}
+		
+		
 	}
 	
 	public void run(){
--- src/Exercise3Tester.java
+++ src/Exercise3Tester.java
@@ -7,17 +7,24 @@ public class Exercise3Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[][] u = new int[2][2]; 
+//		double[][] u = new double[2][2];
+//		
+//		//take U as input
+//		int a = 0;
+//		for (int i=0; i<u.length; i++){ //loop rows
+//			for (int j=0; j<u.length; j++){ //loop columns
+//				u[i][j] = Double.parseDouble(args[a++]);
+//			}
+//		}
 		
-		//take U as input
+		double[] input = new double[4];
 		int a = 0;
-		for (int i=0; i<u.length; i++){ //loop rows
-			for (int j=0; j<u.length; j++){ //loop columns
-				u[i][j] = Integer.parseInt(args[a++]);
-			}
+		for (int i=0; i<input.length; i++){
+			input[i] = Double.parseDouble(args[a++]);
 		}
 		
-		Exercise3 myExercise3 = new Exercise3(u);
+//		Exercise3 myExercise3 = new Exercise3(u);
+		Exercise3 myExercise3 = new Exercise3(input);
 		myExercise3.run();
 	}
 
