28090780e4179b84f52299f45b1ae95ddac6dae8
Jonathan Bourdett <jab2279@columbia.edu>
2012-02-26 20:58:56 -0500

    removed unnecessary comments from Exercise3

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,26 +1,7 @@
 
 public class Exercise3 {
 
-//	private double[][] u;
 	private Complex[][] u;
-	
-//	public Exercise3(double[] input){
-//		u = new double[2][2];
-//		
-//		int a = 0;
-//		int b = 0;
-//		for (int i=0; i<input.length; i++){
-//			if (b>=2){
-//				a++;
-//				b=0;
-//			}
-//			u[a][b++] = input[i];
-//			if (DEBUG == true){
-//				//print out entries of matrix u
-//				System.out.println("u_" + a + (b-1) + " = " + u[a][b-1]);
-//			}
-//		}	
-//	}
 	
 	public Exercise3(Complex[] input){
 		u = new Complex[2][2];
@@ -33,11 +14,8 @@ public class Exercise3 {
 				b=0;
 			}
 			u[a][b++] = input[i];
-//			if (Exercise3Tester.debugging() == true){
-//				//print out entries of matrix u
-//				System.out.println("u_" + a + (b-1) + " = " + u[a][b-1]);
-//			}
 		}	
+		
 		if (Exercise3Tester.debugging() == true){
 			//print out entries of matrix u
 			System.out.println("u matrix");
