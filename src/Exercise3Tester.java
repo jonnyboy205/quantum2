a0018c12ad6861c838327ba31adb47dd593fd8d1
Jonathan Bourdett <jab2279@columbia.edu>
2012-02-26 20:36:21 -0500

    removed comments from Tester.java

--- src/Exercise3Tester.java
+++ src/Exercise3Tester.java
@@ -7,15 +7,6 @@ public class Exercise3Tester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		double[] input = new double[4];
-//		int a = 0;
-//		for (int i=0; i<input.length; i++){
-//			input[i] = Double.parseDouble(args[a++]);
-//		}
-		
-//		Exercise3 myExercise3 = new Exercise3(input);
-		
-		
 		String[] inputStrings = new String[4];
 		Complex[] input = new Complex[4];
 		int a = 0;
@@ -29,7 +20,6 @@ public class Exercise3Tester {
 	}
 
 	private static Complex getComplex(String s) {
-		// TODO Auto-generated method stub
 		Complex ret = new Complex();
 		
 		//let's say I want to parse the second way in the hw
@@ -39,9 +29,6 @@ public class Exercise3Tester {
 		for (int i=0; i<realAndImag.length; i++){
 			realAndImag[i] = realAndImagTemp[i+1];
 		}
-//		for (int i=0; i<realAndImag.length; i++){
-//			realAndImag[i] = removeParens(realAndImag[i]);
-//		}
 		
 		ret.setRe(Double.parseDouble(realAndImag[0]));
 		ret.setIm(Double.parseDouble(realAndImag[1]));
@@ -55,10 +42,5 @@ public class Exercise3Tester {
 	public static boolean debugging(){
 		return debug;
 	}
-
-//	private static String removeParens(String string) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
 
 }
