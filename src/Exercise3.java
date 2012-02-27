597bc474b20a90416fd698f160cdcb1540579509
Jonathan Bourdett <jab2279@columbia.edu>
2012-02-26 20:50:16 -0500

    debug prints nicely. successfully take in complex numbers as input.

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -2,9 +2,9 @@
 public class Exercise3 {
 
 //	private double[][] u;
-//	private boolean DEBUG = true;
+	private Complex[][] u;
 	
-	public Exercise3(double[] input){
+//	public Exercise3(double[] input){
 //		u = new double[2][2];
 //		
 //		int a = 0;
@@ -19,10 +19,37 @@ public class Exercise3 {
 //				//print out entries of matrix u
 //				System.out.println("u_" + a + (b-1) + " = " + u[a][b-1]);
 //			}
-//		}
+//		}	
+//	}
+	
+	public Exercise3(Complex[] input){
+		u = new Complex[2][2];
 		
-		
-	}
+		int a = 0;
+		int b = 0;
+		for (int i=0; i<input.length; i++){
+			if ( b > (u.length-1) ){
+				a++;
+				b=0;
+			}
+			u[a][b++] = input[i];
+//			if (Exercise3Tester.debugging() == true){
+//				//print out entries of matrix u
+//				System.out.println("u_" + a + (b-1) + " = " + u[a][b-1]);
+//			}
+		}	
+		if (Exercise3Tester.debugging() == true){
+			//print out entries of matrix u
+			System.out.println("u matrix");
+			System.out.println("------------");
+			for (int i=0; i<u.length; i++){
+				for (int j=0; j<u.length; j++){
+					System.out.println("u_" + i + j + " = " + u[i][j]);
+				}
+			}
+			System.out.println();
+		}
+}
 	
 	public void run(){
 		
--- src/Exercise3Tester.java
+++ src/Exercise3Tester.java
@@ -14,8 +14,16 @@ public class Exercise3Tester {
 			inputStrings[i] = args[a++];
 			input[i] = getComplex(inputStrings[i]);
 		}
+		if (debugging()){
+			System.out.println("input");
+			System.out.println("--------");
+			for (int i=0; i<input.length; i++){
+				System.out.println(input[i]);
+			}
+			System.out.println();
+		}
 		
-//		Exercise3 myExercise3 = new Exercise3(input);
+		Exercise3 myExercise3 = new Exercise3(input);
 //		myExercise3.run();
 	}
 
@@ -32,9 +40,6 @@ public class Exercise3Tester {
 		
 		ret.setRe(Double.parseDouble(realAndImag[0]));
 		ret.setIm(Double.parseDouble(realAndImag[1]));
-		
-		if (debugging())
-			System.out.println(ret);
 		
 		return ret;
 	}
