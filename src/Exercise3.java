1fb361726ebf698b2ca311cdddb8e23938696555
Jonathan Bourdett <jab2279@columbia.edu>
2012-02-26 19:35:23 -0500

    now taking in input as complex numbers as specified by example format 2 in hw description. need to test

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,25 +1,25 @@
 
 public class Exercise3 {
 
-	private double[][] u;
-	private boolean DEBUG = true;
+//	private double[][] u;
+//	private boolean DEBUG = true;
 	
 	public Exercise3(double[] input){
-		u = new double[2][2];
-		
-		int a = 0;
-		int b = 0;
-		for (int i=0; i<input.length; i++){
-			if (b>=2){
-				a++;
-				b=0;
-			}
-			u[a][b++] = input[i];
-			if (DEBUG == true){
-				//print out entries of matrix u
-				System.out.println("u_" + a + (b-1) + " = " + u[a][b-1]);
-			}
-		}
+//		u = new double[2][2];
+//		
+//		int a = 0;
+//		int b = 0;
+//		for (int i=0; i<input.length; i++){
+//			if (b>=2){
+//				a++;
+//				b=0;
+//			}
+//			u[a][b++] = input[i];
+//			if (DEBUG == true){
+//				//print out entries of matrix u
+//				System.out.println("u_" + a + (b-1) + " = " + u[a][b-1]);
+//			}
+//		}
 		
 		
 	}
--- src/Exercise3Tester.java
+++ src/Exercise3Tester.java
@@ -1,18 +1,57 @@
 
 public class Exercise3Tester {
 
+	private boolean debug;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		double[] input = new double[4];
+//		double[] input = new double[4];
+//		int a = 0;
+//		for (int i=0; i<input.length; i++){
+//			input[i] = Double.parseDouble(args[a++]);
+//		}
+		
+//		Exercise3 myExercise3 = new Exercise3(input);
+		
+		
+		String[] inputStrings = new String[4];
+		Complex[] input = new Complex[4];
 		int a = 0;
 		for (int i=0; i<input.length; i++){
-			input[i] = Double.parseDouble(args[a++]);
+			inputStrings[i] = args[a++];
+			input[i] = getComplex(inputStrings[i]);
 		}
 		
-		Exercise3 myExercise3 = new Exercise3(input);
-		myExercise3.run();
+//		Exercise3 myExercise3 = new Exercise3(input);
+//		myExercise3.run();
 	}
+
+	private static Complex getComplex(String s) {
+		// TODO Auto-generated method stub
+		Complex ret = new Complex();
+		
+		//let's say I want to parse the second way in the hw
+		//with parens
+		String[] realAndImag = s.split("[(,)]");
+//		for (int i=0; i<realAndImag.length; i++){
+//			realAndImag[i] = removeParens(realAndImag[i]);
+//		}
+		
+		ret.setRe(Double.parseDouble(realAndImag[0]));
+		ret.setIm(Double.parseDouble(realAndImag[1]));
+		
+		return ret;
+	}
+	
+	public boolean debugging(){
+		return debug;
+	}
+
+//	private static String removeParens(String string) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 }
